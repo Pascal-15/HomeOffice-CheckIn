c918becb57fb3dcd50ddc78bdbaf6de44da982da
Pascal-K <karsten.pas@gmail.com>
2024-04-18 18:45:08 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { StartComponent } from './start/start.component';
+import { StopComponent } from './stop/stop.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StartComponent,
+    StopComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stop/stop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stop',
+  templateUrl: './stop.component.html',
+  styleUrl: './stop.component.scss'
+})
+export class StopComponent {
+
+}
